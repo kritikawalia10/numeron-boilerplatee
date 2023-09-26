f43df6e0c473291342a12c3877bc72246b43389d
Kritika Walia <kritikawalia987@gmail.com>
2023-09-26 17:33:45 +0530

    updated

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 // Iteration 1: Make the Play button functional.
 // Description: When the Play button is clicked the game page should be displayed.
+// Iteration 1: Make the Play button functional.
+const Play = document.getElementById("play-button");
+
+Play.onclick = ()=>{
+    location.href = "./game.html"
+}
+// Description: When the Play button is clicked the game page should be displayed.
+
--- game.js
+++ game.js
@@ -1,5 +1,73 @@
 // Iteration 2: Generate 2 random number and display it on the screen
+const one = document.getElementById("number1");
+var num1 = Math.round(Math.random()*100);
+one.innerHTML = num1;
+
+const two = document.getElementById("number2");
+var num2 =  Math.round(Math.random()*100);
+two.innerHTML = num2;
 
 // Iteration 3: Make the options button functional
+var score = 0;
+const great = document.getElementById("greater-than");
+great.onclick = ()=>{
+    if(num1>num2){
+       score++;
+       resetTime(timerId);
+    }else{
+        location.href = "./gameover.html";
+    }
+     num1 = Math.round(Math.random()*100);
+    one.innerHTML = num1;
+      num2 =  Math.round(Math.random()*100);
+     two.innerHTML = num2;
+};
+    const equal = document.getElementById("equal-to");
+   equal.onclick = ()=>{
+    if(num1=num2){
+        score++;
+        resetTime(timerId);
+     }else{
+         location.href = "./gameover.html";
+     }
+      num1 = Math.round(Math.random()*100);
+     one.innerHTML = num1;
+      num2 =  Math.round(Math.random()*100);
+      two.innerHTML = num2;
+};
+     const less = document.getElementById("lesser-than");
+      less.onclick = ()=>{
+    if(num1<num2){
+        score++;
+        resetTime(timerId);
+     }else{
+         location.href = "./gameover.html";
+     }
+      num1 = Math.round(Math.random()*100);
+     one.innerHTML = num1;
+      num2 =  Math.round(Math.random()*100);
+     two.innerHTML = num2;
+};
 
 // Iteration 4: Build a timer for the game
+var timer = document.getElementById("timer");
+var time= 5;
+var timerId;
+function startTimer(){
+    time=5;
+    timer.innerHTML = time;
+    timerId= setInterval(()=>{
+        time--;
+        if(time==0){
+            location.href = "./gameover.html";
+        }
+        timer.innerHTML=time;
+    },1000);
+    localStorage.setItem("score",score);
+}
+function resetTime(intervalId){
+    clearInterval(intervalId);
+    startTimer();
+}
+
+startTimer();
